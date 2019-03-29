6691c3a34093975eb16caf88d514f2bb80b7845d
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-03-29 16:24:19 +0100

    Add totp

---
+++ totp.py
@@ -0,0 +1,26 @@
+import base64
+import math
+import hmac
+import time
+import hashlib
+
+
+def totp(priv_key: bytes,
+         digits: int = 6,
+         period: int = 30,
+         unixtime: int = int(time.time())) -> int:
+    raw_key: bytes = base64.b32decode(priv_key)
+    tc: int = math.floor(unixtime / period)
+    return hotp(raw_key, tc)
+
+
+def hotp(key: bytes, msg: int, digits: int = 6) -> int:
+    raw_msg = bytearray(8)
+    for n in range(0, 8):
+        raw_msg[7 - n] = (msg >> n * 8) & 0xff
+    h: bytes = hmac.new(key, raw_msg, hashlib.sha1).digest()
+    offset = h[-1] & 0x0f
+    ubin = (h[offset] << 24) | (h[offset + 1] << 16) | (h[offset + 2] << 8) | (
+        h[offset + 3])
+    sbin = ubin & 0x7fffffff
+    return sbin % pow(10, digits)
