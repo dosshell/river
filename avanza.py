ec617c8539e4d814eead6f130864f59f5bea69d9
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-03-31 19:51:51 +0200

    Add full login_in

--- avanza.py
+++ avanza.py
@@ -4,11 +4,11 @@ import json
 
 class Avanza:
     def __init__(self):
-        is_authed: bool = False           # noqa: F841
-        customer_id: str = ''             # noqa: F841
-        authentication_session: str = ''  # noqa: F841
-        push_subscription_id: str = ''    # noqa: F841
-        security_token: str = ''          # noqa: F841
+        self.is_authed: bool = False
+        self.customer_id: str = ''
+        self.authentication_session: str = ''
+        self.push_subscription_id: str = ''
+        self.security_token: str = ''
 
     def login(self, username: str, password: str, totp_code: str) -> bool:
         if not all([username, password, totp_code]):
@@ -18,17 +18,43 @@ class Avanza:
             'User-Agent': 'Avanza/se.avanzabank.androidapplikation (3.21.2 (585); Android 6.0)',
             'Content-Type': 'application/json; charset=UTF-8'
         }
-        data = {}
-        data['maxInactiveMinutes'] = 240
-        data['password'] = password
-        data['username'] = username
+        data = {
+            'maxInactiveMinutes': 240,
+            'password': password,
+            'username': username
+        }
         response = requests.post(url, data=json.dumps(data), headers=headers)
-        if response.ok:
-            return True
-        elif response.status_code == 401:
+        if response.status_code == 401:
             return False
-        else:
+        elif not response.ok:
             raise ValueError('Some error here', response.status_code)
+        if not response.headers['Content-Type'] == 'application/json;charset=utf-8':
+            return False
+
+        content = json.loads(response.content)
+        transaction_id = content['twoFactorLogin']['transactionId']
+        url = 'https://www.avanza.se/_api/authentication/sessions/totp'
+        headers = {
+            'User-Agent': 'Avanza/se.avanzabank.androidapplikation (3.21.2 (585); Android 6.0)',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'VND.se.avanza.security-Totp-Transaction-Id': transaction_id
+        }
+        data = {
+            "maxInactiveMinutes": 240,
+            "method": "TOTP",
+            "totpCode": totp_code
+        }
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        if not response.ok:
+            return False
+        content = json.loads(response.content)
+
+        self.is_authed = True
+        self.customer_id = content['customerId']
+        self.authentication_session = content['authenticationSession']
+        self.push_subscription_id = content['pushSubscriptionId']
+        self.security_token = response.headers['X-SecurityToken']
+        return True
 
     def get_instrument(self, instrument_id: str, period: str = 'five_years') -> None:
         pass
