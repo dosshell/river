d6e691430678dcf984e15a1e3423ad544b0ea519
Markus Lindelöw <markus@lindeloew.se>
2019-04-09 20:13:31 +0200

    Add schedule

--- daemon.py
+++ daemon.py
@@ -1,1 +1,19 @@
-print("Hello World from River daemon.")
+import schedule
+import time
+
+
+def job():
+    print("Doing stuff")
+
+
+def main():
+    print("Unleashing the daemon of River Tam")
+    schedule.every().day.at("9:00").do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
