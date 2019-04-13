5c162780ca07cd8654e77ebbb92dc7a2999b3460
Markus Lindelöw <markus@lindeloew.se>
2019-04-13 15:46:28 +0200

    Refactor tests

--- avanza.py
+++ avanza.py
@@ -1,5 +1,10 @@
 import requests
 import json
+import unittest
+from settings import config as cfg
+from settings import is_template as is_template_settings
+import totp
+import warnings
 
 
 class Avanza:
@@ -97,3 +102,27 @@ class Avanza:
             return json.loads(response.content)
         else:
             return None
+
+
+class TestAvanzaApi(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.avanza_client = Avanza()
+        if (is_template_settings):
+            warnings.warn("Can not test Avanza with template settings.")
+
+    def step0_test_login(self):
+        if (not is_template_settings):
+            username = cfg['AvanzaUsername']
+            password = cfg['AvanzaPassword']
+            priv_key = cfg['AvanzaPrivateKey']
+
+            totp_code = totp.totp(priv_key)
+            self.assertTrue(self.avanza_client.login(
+                username, password, totp_code))
+
+    def step1_test_overview(self):
+        overview = self.avanza_client.get_overview()
+        self.assertIsNotNone(overview)
+        for account in overview['accounts']:
+            self.assertIsNotNone(self.avanza_client.get_account_overview(account['accountId']))
