64b9548b1507b36b9772aea19b24bcf4b012b22b
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-07-29 15:24:20 +0200

    Add simple db interface

--- src/db.py
+++ src/db.py
@@ -1,43 +1,38 @@
 import sqlite3
-import avanza
 
 
-def update(avanza_client: avanza.Avanza):
-    if (not avanza_client.is_authed):
-        print('No avanza connection')
-        return None
+class Database:
+    '''A simple database interface'''
 
-    overview = avanza_client.get_overview()
-    accounts = [(x['accountId'], x['accountType'], str(x['depositable'])) for x in overview['accounts']]
-    balance = [(x['accountId'], x['ownCapital']) for x in overview['accounts']]
+    def __init__(self):
+        self._conn = None
 
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
+    def open(self, db_file: str, non_persistent: bool = False):
+        if not non_persistent:
+            self._conn = sqlite3.connect(db_file)
+        else:
+            source = sqlite3.connect(db_file)
+            self._conn = sqlite3.connect(':memory:')
+            source.backup(self._conn)
+            source.close()
 
-    # Create structure
-    c.execute('''CREATE TABLE IF NOT EXISTS
-                 account (
-                   account_id NOT NULL PRIMARY KEY,
-                   account_type,
-                   depositable
-                 )
-          ''')
+    def close(self):
+        self._conn.close()
 
-    c.execute('''CREATE TABLE IF NOT EXISTS
-                 balance (
-                   account_id,
-                   total_balance,
-                   datetime,
-                   FOREIGN KEY(account_id) REFERENCES account(account_id)
-                 )
-          ''')
+    def backup(self, backup_file: str) -> bool:
+        if self._conn is None:
+            return False
 
-    # Store data
-    c.executemany('''INSERT OR IGNORE INTO account VALUES (?, ?, ?)''', accounts)
-    c.executemany('''INSERT INTO balance VALUES (?, ?, datetime('now'))''', balance)
+        bck = sqlite3.connect(backup_file)
+        self._conn.backup(bck)
+        bck.close()
+        return True
 
-    conn.commit()
+    def insert_account_sample(self, account_sample):
+        pass
 
-    conn.close()
+    def update_instrument_list(self, list_of_instruments):
+        pass
 
-    return True
+    def update_instrument(self, instrument_log):
+        pass
