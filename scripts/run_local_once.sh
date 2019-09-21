8ec7a6fd3fbb2365ba0f03fa6fcdecb56b08012c
Markus Lindelöw <markus@lindeloew.se>
2019-09-21 16:02:38 +0200

    Pass by arguments in scripts

--- scripts/run_local_once.sh
+++ scripts/run_local_once.sh
@@ -5,4 +5,4 @@ cd $MY_PATH/../src
 
 python3 -m pip install pipenv
 python3 -m pipenv sync
-python3 -m pipenv run python daemon.py --now -c ../settings.json
+python3 -m pipenv run python daemon.py --now -c ../settings.json "$@"
