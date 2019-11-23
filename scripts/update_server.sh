21b2b611ac61d4bf540e24ccca0d78c9397d92f3
Markus Lindelöw <markus@lindeloew.se>
2019-11-23 16:46:48 +0100

    Simplify scripts

--- scripts/update_server.sh
+++ scripts/update_server.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-# run this file on the server to update to latest
-
 MY_PATH="`dirname \"$0\"`"
 cd $MY_PATH/..
 
 git pull
-./scripts/build_push_image.sh
-./scripts/update_container.sh
-
+./scripts/start_server.sh
