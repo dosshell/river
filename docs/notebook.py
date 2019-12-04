4082d7477c250a81bd99428d3e22a0f2590700d0
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-12-04 11:25:42 +0100

    Add estimate error to notebook

--- docs/notebook.py
+++ docs/notebook.py
@@ -24,7 +24,8 @@ px.line(x=t, y=z)
 px.line(x=t, y=z, log_y=True)
 
 # %% Kalman
-x = s2.cv_kf(t, z, 0.001)
+
+x = s2.cv_kf(t, z, 0.005)
 x0 = x[:, 0, 0]
 
 fig = go.Figure()
@@ -33,5 +34,19 @@ fig.add_trace(go.Scatter(x=t, y=z, mode='lines'))
 fig.add_trace(go.Scatter(x=t, y=x0, mode='lines'))
 fig.update_layout(yaxis_type="log")
 fig.show()
+
+# %% Estimate Error
+
+t_bar, z_hat, z_bar = s2.cv_kf_estimate(t, z, 0.1, np.timedelta64(21, 'D'))
+
+e = (z_hat - z_bar) / z[range(len(z_hat))]
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=t_bar, y=z_bar, mode='lines'))
+fig.add_trace(go.Scatter(x=t_bar, y=z_hat, mode='lines'))
+fig.update_layout(yaxis_type="log")
+fig.show()
+
+px.line(y=e)
 
 # %%
