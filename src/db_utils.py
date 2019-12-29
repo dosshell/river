0c0d492d526819e06fd7d2bfd80e4ec519600305
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-12-29 13:27:46 +0100

    Use connections for db_utils

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,42 +1,46 @@
-# import apsw # APSW has no apsw.Cursor attribute bug
+import apsw
 import pandas
+from typing import Tuple, List
 
 
-def sql_to_df(cursor, table_name: str) -> pandas.DataFrame:
-    rh = cursor.execute(f'''PRAGMA table_info({table_name})''').fetchall()
+def sql_to_df(conn: apsw.Connection, table_name: str) -> pandas.DataFrame:
+    c = conn.cursor()
+    rh = c.execute(f'''PRAGMA table_info({table_name})''').fetchall()
     headers = [x[1] for x in rh]
-    r = cursor.execute(f'''select * from {table_name}''')
+    r = c.execute(f'''select * from {table_name}''')
     data = r.fetchall()
     df = pandas.DataFrame.from_records(data, columns=headers)
     return df
 
 
-def df_to_sql(cursor, table_name: str, df: pandas.DataFrame) -> None:
+def df_to_sql(conn: apsw.Connection, table_name: str, df: pandas.DataFrame) -> None:
     qs = ','.join(['?'] * len(df.columns))
     cs = ','.join(df.columns.tolist())
-    cursor.executemany(f'''insert or ignore into {table_name} ({cs}) values({qs})''', df.values.tolist())
+    conn.cursor().executemany(f'''insert or ignore into {table_name} ({cs}) values({qs})''', df.values.tolist())
 
 
-def tuplelist_to_sql(cursor, table_name: str, values: pandas.DataFrame) -> None:
+def tuplelist_to_sql(conn: apsw.Connection, table_name: str, values: List[Tuple]) -> None:
     if len(values) < 1:
         return
     columns = len(values[0])
     qs = ','.join(['?'] * columns)
-    cursor.execute("BEGIN TRANSACTION")
-    cursor.executemany(f'''insert or ignore into {table_name} values ({qs})''', values)
-    cursor.execute("COMMIT")
+    c = conn.cursor()
+    c.execute("BEGIN TRANSACTION")
+    c.executemany(f'''insert or ignore into {table_name} values ({qs})''', values)
+    c.execute("COMMIT")
 
 
-def create_tables(cursor) -> None:
-    cursor.execute('''CREATE TABLE IF NOT EXISTS fund_list(
-                        orderbook_id INTEGER PRIMARY KEY NOT NULL,
-                        name TEXT NOT NULL UNIQUE,
-                        start_date TEXT NOT NULL
-                      )''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS fund_chart(
-                        orderbook_id INTEGER NOT NULL,
-                        x TEXT NOT NULL,
-                        y REAL,
-                        PRIMARY KEY(orderbook_id, x),
-                        FOREIGN KEY(orderbook_id) REFERENCES fund_list(orderbook_id)
-                      )''')
+def create_tables(conn: apsw.Connection) -> None:
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS fund_list(
+                    orderbook_id INTEGER PRIMARY KEY NOT NULL,
+                    name TEXT NOT NULL UNIQUE,
+                    start_date TEXT NOT NULL
+                    )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS fund_chart(
+                    orderbook_id INTEGER NOT NULL,
+                    x TEXT NOT NULL,
+                    y REAL,
+                    PRIMARY KEY(orderbook_id, x),
+                    FOREIGN KEY(orderbook_id) REFERENCES fund_list(orderbook_id)
+                    )''')
