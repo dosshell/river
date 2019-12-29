a457cdfd50bad4db1c8ca13f46d5f496a48a887d
Markus Lindelöw <markus.lindelow@smarteye.se>
2019-12-29 13:16:08 +0100

    Update test data

--- src/test/create_test_data.py
+++ src/test/create_test_data.py
@@ -2,12 +2,11 @@ import argparse
 import requests
 import json
 import os
-import datetime
-import apsw
-from db_utils_link import db_utils
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + '/data'
 
 
-def get_test_data_fund_list(fund_filter):
+def create_fund_list_data(fund_filter):
     url = 'https://www.avanza.se/_cqbe/fund/list'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
@@ -47,23 +46,25 @@ def get_test_data_fund_list(fund_filter):
         r = response.json()
         new_funds = [x for x in r['fundListViews'] if x['orderbookId'] in fund_filter]
         data['fundListViews'].extend(new_funds)
-    return data
+
+    with open(dir_path + '/fund_list.json', 'w') as f:
+        json.dump(data, f)
 
 
-def get_test_data_fund(orderbook_id):
+def create_fund_data(orderbook_id):
     '''Only support full years for now'''
     url = f'''https://www.avanza.se/_mobile/market/fund/{orderbook_id}'''
     headers = {'User-Agent': 'Avanza API client/1.3.0', 'Content-Type': 'application/json; charset=UTF-8'}
     response = requests.get(url, headers=headers)
     if not response.ok:
         raise ValueError("Connection problems?")
-    return response.json()
+
+    with open(dir_path + f"""/fund_{orderbook_id}.json""", 'w') as f:
+        json.dump(response.json(), f)
 
 
-def get_test_data_fund_chart(orderbook_id, year):
-    from_date = datetime.date(year, 1, 1).isoformat()
-    to_date = datetime.date(year + 1, 1, 1).isoformat()
-    url = f'''https://www.avanza.se/_cqbe/fund/chart/{orderbook_id}/{from_date}/{to_date}'''
+def create_fund_chart_data(orderbook_id, start_date, stop_date):
+    url = f'''https://www.avanza.se/_cqbe/fund/chart/{orderbook_id}/{start_date}/{stop_date}'''
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
         'Content-Type': 'application/json; charset=UTF-8'
@@ -71,58 +72,33 @@ def get_test_data_fund_chart(orderbook_id, year):
     response = requests.get(url, headers=headers)
     if not response.ok:
         raise ValueError("Connection problems?")
-    return response.json()
 
-
-def create_test_db(conn, fund_list_data, chart_datas):
-    with conn:
-        cursor = conn.cursor()
-        db_utils.create_tables(cursor)
-
-        # Populate with real data from created json files
-        values = [(x['orderbookId'], x['name'], x['startDate']) for x in fund_list_data['fundListViews']]
-        cursor.executemany('insert into fund_list values (?,?,?)', values)
-        for fund, data in chart_datas.items():
-            pfix = 1.0
-            for d in data:
-                rows = [(fund, datetime.datetime.utcfromtimestamp(x['x'] / 1000).isoformat(), (x['y'] / 100 + 1) * pfix)
-                        for x in d['dataSerie'] if x['y'] is not None]
-                pfix = rows[-1][2]
-                cursor.executemany('insert into fund_chart values(?,?,?)', rows)
+    start_date_short = start_date.replace('-', '')
+    stop_date_short = stop_date.replace('-', '')
+    with open(dir_path + f"""/fund_chart_{orderbook_id}_{start_date_short}_{stop_date_short}.json""", 'w') as f:
+        json.dump(response.json(), f)
 
 
 def main(args):
-    funds = [944976, 377804]
+    funds = [377804, 944976]
 
-    dir_path = os.path.dirname(os.path.realpath(__file__)) + '/data'
+    create_fund_list_data(funds)
 
-    # create/overrite fund_list.json
-    fund_list_data = get_test_data_fund_list(funds)
-    with open(dir_path + '/fund_list.json', 'w') as f:
-        json.dump(fund_list_data, f)
+    for n in funds:
+        create_fund_data(n)
 
-    fund_chart_datas = {}
-    for orderbook_id in funds:
-        fund_data = get_test_data_fund(orderbook_id)
-        with open(dir_path + f'''/fund_{orderbook_id}.json''', 'w') as f:
-            json.dump(fund_data, f)
-
-        from_year = int(fund_data['startDate'].split('-')[0])
-        to_year = datetime.date.today().year + 1
-        fund_chart_datas[orderbook_id] = []
-        for year in range(from_year, to_year):
-            chart_data = get_test_data_fund_chart(orderbook_id, year)
-            fund_chart_datas[orderbook_id].append(chart_data)
-            with open(dir_path + f'''/fund_chart_{orderbook_id}_{year}.json''', 'w') as f:
-                json.dump(chart_data, f)
-
-    # populate with test data
-    db_path = dir_path + '/test.db'
-    if os.path.isfile(db_path):
-        os.remove(db_path)
-    conn = apsw.Connection(db_path)
-    create_test_db(conn, fund_list_data, fund_chart_datas)
-    conn.close()
+    create_fund_chart_data(377804, "2012-10-19", "2012-10-19")
+    create_fund_chart_data(377804, "2012-10-20", "2013-10-15")
+    create_fund_chart_data(377804, "2013-10-16", "2014-10-11")
+    create_fund_chart_data(377804, "2014-10-12", "2015-10-07")
+    create_fund_chart_data(377804, "2015-10-08", "2016-10-02")
+    create_fund_chart_data(377804, "2016-10-03", "2017-09-28")
+    create_fund_chart_data(377804, "2017-09-29", "2018-09-24")
+    create_fund_chart_data(377804, "2018-09-25", "2019-09-20")
+    create_fund_chart_data(377804, "2019-09-21", "2019-12-28")
+    create_fund_chart_data(377804, "2019-12-24", "2019-12-29")
+    create_fund_chart_data(944976, "2019-04-01", "2019-04-01")
+    create_fund_chart_data(944976, "2019-04-02", "2019-12-28")
 
 
 if __name__ == '__main__':
