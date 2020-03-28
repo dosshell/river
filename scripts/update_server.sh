6d821f4f126005204f29e4751bd8f7e4adea7aa6
Markus Lindelöw <markus@lindeloew.se>
2020-03-28 09:09:40 +0100

    Use run_server.sh script
    update_server used latest pushed image. run_server will use current git
    checkout instead.

--- scripts/update_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MY_PATH="`dirname \"$0\"`"
-cd $MY_PATH/..
-
-git pull
-echo "You need to migrate config.json and auth.json manually now."
-read -p "Press enter when done"
-./scripts/start_server.sh
